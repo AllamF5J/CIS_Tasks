1464701bbd2ae499613b7f8f277b9f874cf24931
Ahmed Allam <lamahmd122@gmail.com>
2024-03-04 21:23:46 +0200

    server.h

---
+++ beginnerLevel/Final Project/Server/server.h
@@ -0,0 +1,9 @@
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+void recieveTransactionData(void);
+void isValidAccount(void);
+void isAmountAvailable(void);
+void saveTransaction(void);
+
+#endif // !_SERVER_H_
