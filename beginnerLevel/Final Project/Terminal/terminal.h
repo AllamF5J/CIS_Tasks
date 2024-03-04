cde778b01703eb33c0b32142303d8762f0f4fa20
Ahmed Allam <lamahmd122@gmail.com>
2024-03-04 21:24:27 +0200

    terminal.h

---
+++ beginnerLevel/Final Project/Terminal/terminal.h
@@ -0,0 +1,10 @@
+#ifndef _TERMINAL_H_
+#define _TERMINAL_H_
+
+void getTransactionDate(void);
+void isCardExpried(void);
+void getTransactionAmount(void);
+void isBelowMaxAmount(void);
+void setMaxAmount(void);
+
+#endif // !_TERMINAL_H_
