d26303400d5a57bec66f46f523728b9e6a73d85b
Ahmed Allam <lamahmd122@gmail.com>
2024-03-04 21:24:59 +0200

    Basics Of Code

---
+++ beginnerLevel/Final Project/main.c
@@ -0,0 +1,41 @@
+/**
+*****************************************************************************
+* @file           : Payment Application
+* @author         : Ahmed Allam
+* @brief          : An application that enables the user to carry out simple financial transactions,
+*                   consists of 3 main modules
+* @details        : 
+* 
+*                   Project Features:
+*                   - 
+*                   - 
+*                   - 
+*                   - 
+*                   - 
+* 
+*                   Note: 
+*
+* @version         : 00.00.00
+*****************************************************************************
+*/
+
+#include <stdio>
+
+#include "Card/card.h"
+#include "Terminal/terminal.h"
+#include "Server/server.h"
+#include "Application/application.h"
+#include "type.h"
+
+int main()
+{
+
+  return 0;
+}
+
+/**
+*****************************************************************************
+*  User                   Date                 Brief
+*****************************************************************************
+*  GziXnine               04/03/2024           Make structure of basic code.
+*/
