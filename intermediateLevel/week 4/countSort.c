beaf78dfffbd4d32ec682694c4c0c4c2458dab57
Ahmed Allam <60698204+AllamF5J@users.noreply.github.com>
2024-04-22 04:17:24 +0200

    Count Sort

--- intermediateLevel/week 4/countSort.c
+++ intermediateLevel/week 4/countSort.c
@@ -8,7 +8,7 @@ void printArray(signed int array[], unsigned int size);
 
 int main ()
 {
-  // If you want to check the code remove the multiline comment from ( 11 ~ 21 ) line.
+  // If you want to check the code remove the multiline comment from ( 12 ~ 22 ) line.
   /*
   int arr[] = {15, 7, 5, 12, 3, 10, 2, 8, 4, 1, 14, 9, 11, 6, 13};
 
@@ -28,23 +28,32 @@ void checkMax(signed int array[], signed int size)
   signed int max = array[0];
   signed int index = 0;
 
-  for (index = 0; index < size - 1; ++index)
+  // Find the maximum element in the array
+  for (index = 0; index < size; ++index)
   {
-    if (array[index] > array[0])
+    if (array[index] > max)
     {
       max = array[index];
     }
   }
+  // Call CountSort function with the maximum element and the size of the array
   CountSort(array, max, size);
 }
 
 void CountSort(signed int array[], signed int maxElement, signed int size)
 {
+  signed int index = 0;
+
   // This is not allowed in C standard to declare an array with sizes determined at runtime like an 'maxElement' or 'size'...
   // I should using dynamic memory allocate.
   signed int *count = (signed int *)calloc((size_t)(maxElement + 1), sizeof(signed int));
   signed int *copyArray = (signed int *)calloc((size_t)size, sizeof(signed int));
-  signed int index = 0;
+
+  if(!count || !copyArray)
+  {
+    // Memory allocation failed
+    return;
+  }
 
   for (index = 0; index < size; ++index)
   {
@@ -61,6 +70,10 @@ void CountSort(signed int array[], signed int maxElement, signed int size)
   {
     array[--count[copyArray[index]]] = copyArray[index];
   }
+
+  // Free dynamically allocated memory
+  free(count);
+  free(copyArray);
 }
 
 void printArray(signed int array[], unsigned int size)
